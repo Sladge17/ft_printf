3dadab7092b633094a5bc0226cdd3160ff8bd6ed
Jommo Thuy <jthuy@ga-g1.21-school.ru>
2019-11-12 21:42:31 +0300

    fix error width

---
+++ .vscode/compile.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+FILES=$(find $1 -type f -name *.c -print)
+# gcc -g -Wall -Wextra -Werror $FILES -o $1/a.out
+gcc -g $FILES -o $1/a.out
--- def_lengths.c
+++ def_lengths.c
@@ -6,7 +6,7 @@
 /*   By: jthuy <jthuy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 13:12:48 by jthuy             #+#    #+#             */
-/*   Updated: 2019/11/12 20:44:22 by jthuy            ###   ########.fr       */
+/*   Updated: 2019/11/12 21:39:48 by jthuy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ int		def_width(const char **str)
 		width = width * 10 + (**str - '0');
 		*str += 1;
 	}
-	if (**str == 'u')
-		width += 1;
 	return (width);
 }
 
