1b07e310260a27c3fd9185928d2442b17e35d4ea
Jommo Thuy <jthuy@ga-g1.21-school.ru>
2019-11-22 12:52:42 +0300

    big refactoring ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jthuy <jthuy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:32:14 by jthuy             #+#    #+#             */
-/*   Updated: 2019/11/22 12:44:14 by jthuy            ###   ########.fr       */
+/*   Updated: 2019/11/22 12:51:20 by jthuy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,32 @@ int		ft_printf(const char *str, ...)
 		flags = check_flags(&str);
 		width = def_width(&str);
 		value = va_arg(args, void *);
+		
+		if (*str == 'o')
+		{
+			value =binto_oct((int)value);
+			flags |= 32;
+		}
+		
+		if (*str == 'u')
+			flags |= 32;
 
+		if (*str == 'd' || *str == 'i' || flags & 32)
+		{	
+			if (width && !(flags & 17))
+				put_space(len_space(width, &str, &value, flags), flags, &amount);
+			put_sign((int)value, flags, &amount);
+			if (width && flags & 16 && !(flags & 1))
+				put_space(len_space(width, &str, &value, flags), flags, &amount);
+			put_abs((int)value, flags, &amount);
+			put_prefix((int)value, flags, &amount);
+			put_uabs((int)value, flags, &amount);
+			if (width && flags & 1)
+				put_space(len_space(width, &str, &value, flags), flags, &amount);
+			str += 1;
+			continue ;
+		}	
+		
 		if (*str == 's')
 		{
 			if (!value)
@@ -56,133 +81,6 @@ int		ft_printf(const char *str, ...)
 			str += 1;
 			continue ;
 		}
-		
-		if (*str == 'o')
-		{
-			value =binto_oct((int)value);
-			flags |= 32;
-		}
-		
-		if (*str == 'u')
-			flags |= 32;
-
-		if (*str == 'd' || *str == 'i' || flags & 32)
-		{	
-			if (width && !(flags & 17))
-				put_space(len_space(width, &str, &value, flags), flags, &amount);
-			put_sign((int)value, flags, &amount);
-			if (width && flags & 16 && !(flags & 1))
-				put_space(len_space(width, &str, &value, flags), flags, &amount);
-			put_abs((int)value, flags, &amount);
-			put_prefix((int)value, flags, &amount);
-			put_uabs((int)value, flags, &amount);
-			if (width && flags & 1)
-				put_space(len_space(width, &str, &value, flags), flags, &amount);
-			str += 1;
-			continue ;
-		}
-
-		// if (*str == 'd' || *str == 'i' || flags & 32)
-		// {		
-		// 	if (!width)
-		// 	{
-		// 		put_sign((int)value, flags, &amount);
-		// 		put_abs((int)value, flags, &amount);
-		// 		put_prefix((int)value, flags, &amount);
-		// 		put_uabs((int)value, flags, &amount);
-		// 		// if (!(flags & 32))
-		// 		// {
-		// 		// 	put_sign((int)value, flags, &amount);
-		// 		// 	// put_abs((int)value, &amount);
-		// 		// 	put_abs((int)value, flags, &amount);
-		// 		// }
-		// 		// if (flags & 32)
-		// 		// {
-		// 		// 	// put_prefix(&amount, flags);
-		// 		// 	// put_uabs((int)value, &amount);
-		// 		// 	put_prefix((int)value, flags, &amount);
-		// 		// 	put_uabs((int)value, flags, &amount);
-		// 		// }
-		// 		str += 1;
-		// 		continue ;
-		// 	}
-
-		// 	if (flags & 1)
-		// 	{
-		// 		// if (!(flags & 32))
-		// 		// {
-		// 		// 	put_sign((int)value, flags, &amount);
-		// 		// 	// put_abs((int)value, &amount);
-		// 		// 	put_abs((int)value, flags, &amount);
-		// 		// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 		// }
-		// 		// if (flags & 32)
-		// 		// {
-		// 		// 	// put_prefix(&amount, flags);
-		// 		// 	// put_uabs((int)value, &amount);
-		// 		// 	put_prefix((int)value, flags, &amount);
-		// 		// 	put_uabs((int)value, flags, &amount);
-		// 		// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 		// }
-		// 		put_sign((int)value, flags, &amount);
-		// 		put_abs((int)value, flags, &amount);
-		// 		put_prefix((int)value, flags, &amount);
-		// 		put_uabs((int)value, flags, &amount);
-		// 		put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 		str += 1;
-		// 		continue ;
-		// 	}
-				
-		// 	if (flags & 16)
-		// 	{
-		// 		// if (!(flags & 32))
-		// 		// {
-		// 		// 	put_sign((int)value, flags, &amount);
-		// 		// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 		// 	// put_abs((int)value, &amount);
-		// 		// 	put_abs((int)value, flags, &amount);
-		// 		// }
-		// 		// if (flags & 32)
-		// 		// {
-		// 		// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 		// 	// put_prefix(&amount, flags);
-		// 		// 	// put_uabs((int)value, &amount);
-		// 		// 	put_prefix((int)value, flags, &amount);
-		// 		// 	put_uabs((int)value, flags, &amount);
-		// 		// }
-		// 		put_sign((int)value, flags, &amount);
-		// 		put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 		put_abs((int)value, flags, &amount);
-		// 		put_prefix((int)value, flags, &amount);
-		// 		put_uabs((int)value, flags, &amount);
-		// 		str += 1;
-		// 		continue ;
-		// 	}
-			
-		// 	// if (!(flags & 32))
-		// 	// {
-		// 	// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 	// 	put_sign((int)value, flags, &amount);
-		// 	// 	// put_abs((int)value, &amount);
-		// 	// 	put_abs((int)value, flags, &amount);
-		// 	// }
-		// 	// if (flags & 32)
-		// 	// {
-		// 	// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 	// 	// put_prefix(&amount, flags);
-		// 	// 	// put_uabs((int)value, &amount);
-		// 	// 	put_prefix((int)value, flags, &amount);
-		// 	// 	put_uabs((int)value, flags, &amount);
-				
-		// 	// }
-		// 	put_space(len_space(width, &str, &value, flags), flags, &amount);
-		// 	put_sign((int)value, flags, &amount);
-		// 	put_abs((int)value, flags, &amount);
-		// 	put_prefix((int)value, flags, &amount);
-		// 	put_uabs((int)value, flags, &amount);
-		// 	str += 1;
-		// 	continue ;
-		// }	
 		
 		if (*str == 'c')
 		{
