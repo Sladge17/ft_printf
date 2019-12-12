79290725ab2ba0afd6e09474daf5721e5ec5738b
Jommo Thuy <jthuy@il-h5.21-school.ru>
2019-12-12 14:00:48 +0300

    add executing base part of f

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jthuy <jthuy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 12:32:14 by jthuy             #+#    #+#             */
-/*   Updated: 2019/12/11 20:48:55 by jthuy            ###   ########.fr       */
+/*   Updated: 2019/12/12 13:16:50 by jthuy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	read_double(long double value)
 	unsigned short	exp;
 	unsigned long	mantissa;
 	int				i;
+	int				rez;
 
 	ptr = &value;
 	i = 64;
@@ -36,13 +37,32 @@ void	read_double(long double value)
 	// exp = (exp >> 1) - 16383;
 	exp = *((unsigned long *)ptr + 1);
 	exp = exp & 32768 ? (exp ^ 32768) -16383 : exp - 16383;
-	printf("\n%hu\n", exp);
-	
+	printf("\nexp = %hu\n", exp);
 	mantissa = *(unsigned long *)ptr;
-	printf("%lu\n", mantissa);
+	// printf("%lu\n", mantissa);
+	
+	i = 64;
 	while (mantissa && !(mantissa & 1UL))
+	{
 		mantissa >>= 1;
-	printf("%lu\n", mantissa);
+		i -= 1;
+	}
+	// printf("%lu\n", mantissa);
+	printf("%d\n", i);
+	rez = 0;
+	while (exp > 0)
+	{
+		if (mantissa & 1UL << (i - 1))
+			rez = rez + (1 << exp);
+		exp -= 1;
+		i -= 1;
+	}
+	rez = mantissa & 1UL << (i - 1) ? rez + 1 : rez;
+	
+	printf("rez = ");
+	if (*((unsigned long *)ptr + 1) & 1UL << 15)
+		printf("-");
+	printf("%d.\n", rez);
 }
 
 // void	read_double(double value)
