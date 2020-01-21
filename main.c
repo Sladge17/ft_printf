6c58f99aea535cfbeab54d7790d52e36c78fc5a1
Jommo Thuy <jthuy@ga-g3.21-school.ru>
2020-01-21 12:06:48 +0300

    change func put_freesmb

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jthuy <jthuy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 17:12:02 by jthuy             #+#    #+#             */
-/*   Updated: 2020/01/20 18:23:45 by jthuy            ###   ########.fr       */
+/*   Updated: 2020/01/21 12:01:09 by jthuy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,53 @@
 
 int		ft_printf(const char *str, ...);
 
-int		main()
+int		main(int argc, char **argv)
 {
-	int		test;
+	// int		test;
 	int		rez;
 	
-	char	c;
-	char	*name;
-	int		numb;
+	// char	c;
+	// char	*name;
+	// int		numb;
 
-	char	*q1;
-	char	*q2;
-	char	*q3;
-	
-	q1 = "test1";
-	q2 = "test2";
-	q3 = "test3";
+	// char	*q1;
+	// char	*q2;
+	// char	*q3;
 
-	c = 'w';
-	name = "School";
-	numb = 42;
+	
+	
+	// q1 = "test1";
+	// q2 = "test2";
+	// q3 = "test3";
 
-	test = 0;
-	
-	char	*test_simple_mix;
+	// c = 'w';
+	// name = "School";
+	// numb = 42;
 
-	test_simple_mix = "Hello World";
+	// test = 0;
 	
-	test += 1;
+	// char	*test_simple_mix;
 
-	printf("\n");
-	printf("test %d  <----------\n", test);
-	rez = printf("%s", q1);
+	// test_simple_mix = "Hello World";
 	
-	printf("->%d\n", rez);
-	printf("---\n");
-	fflush(NULL);
-	// rez = ft_printf("{red}%s{/} %s", q1, q2);
-	rez = ft_printf("{magenta}%s{/} {red}%s", q1, q2);
+	// test += 1;
+
+	// printf("\n");
+	// printf("test %d  <----------\n", test);
+	// rez = printf("%s", q1);
 	
-	write(1, "\e[0m", 4);
+	// printf("->%d\n", rez);
+	// printf("---\n");
+	// fflush(NULL);
+	// // rez = ft_printf("{red}%s{/} %s", q1, q2);
+	// rez = ft_printf("{magenta}%s{/} {red}%s", q1, q2);
+	
+	// write(1, "\e[0m", 4);
+	// printf("->%d\n", rez);
+
+	if (argc == 1)
+		return (0);
+	rez = ft_printf(argv[1]);
 	printf("->%d\n", rez);
 
 	return (0);
